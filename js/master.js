3b9de0b2399876dd9d1e09ff65991c5713a07511
Francisco Innocenti <franciscoinnocenti@fmbp.local>
2018-04-16 20:01:13 -0300

    preparing modals

--- js/master.js
+++ js/master.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
-  $('.parallax').parallax();
+  $('.modal').modal();
   $('.collapsible').collapsible();
   $('.sidenav').sidenav();
+  $('textarea').characterCounter();
   $('.btn-show').on('click',function(){
     $(this).parents().siblings().removeClass('hide-on-med-and-down');
     $(this).addClass('hide-on-med-and-down');
@@ -19,48 +20,9 @@ $(document).ready(function() {
     }
   });
 });
-// Hide .sidenav-trigger on on scroll down
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = $('.sidenav-trigger').outerHeight();
-
-$(window).scroll(function(event){
-    didScroll = true;
-});
-
-setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
-}, 250);
-
-function hasScrolled() {
-    var st = $(this).scrollTop();
-
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-        return;
-
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('.sidenav-trigger').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('.sidenav-trigger').removeClass('nav-up');
-        }
-    }
-
-    lastScrollTop = st;
-}
 
 // Google Analytics
-window.dataLayer = window.dataLayer || [];
+  window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-
   gtag('config', 'UA-117686071-1');
