8fe8ebbb86558145048d1149b4964d7d65328acb
Francisco Innocenti <franciscoinnocenti@fmbp.local>
2018-06-13 11:44:40 +0300

    d

--- js/gtranslate.js
+++ js/gtranslate.js
@@ -1,3 +1,3 @@
 function googleTranslateElementInit() {
-  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true, gaId: 'UA-118237284-1'}, 'google_translate_element');
+  new google.translate.TranslateElement({pageLanguage: 'es', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, gaTrack: true, gaId: 'UA-118237284-1'}, 'google_translate_element');
 }
