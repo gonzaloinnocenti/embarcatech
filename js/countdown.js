71e908a2985f7c2f45fe08853d0d51a386932c83
Francisco Innocenti <franciscoinnocenti@fmbp.local>
2018-06-15 19:32:13 +0300

    update countdown

--- js/countdown.js
+++ js/countdown.js
@@ -5,7 +5,7 @@ const second = 1000,
 var callflag = true;
 
 let countDown = new Date('aug 14, 2018 00:00:00').getTime(),
- maxCountDown = new Date('aug 16, 2018 12:30:00').getTime(),
+ maxCountDown = new Date('aug 16, 2018 00:00:00').getTime(),
   x = setInterval(function() {
     let now = new Date().getTime(),
       distance = countDown - now;
